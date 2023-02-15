29d8bb858d1265b8c7a575bbd5f10f0ada2038e2
shubham <shubham25122001@gmail.com>
2023-02-15 08:50:46 +0530

    Lecture 28

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ export class NavBar extends Component {
     render() {
         return (
             <div>
-              <nav className="navbar navbar-expand-lg bg-body-tertiary">
+              <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
               <div className="container-fluid">
                 <a className="navbar-brand" href="/">Navbar</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
